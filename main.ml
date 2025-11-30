d04bd4330f026d067e81703a9098ee2e3938c9f3
John David Pressman <jdpostage@linuxmail.org>
2025-11-30 04:20:27 -0800

    Update main.ml and pray

--- main.ml
+++ main.ml
@@ -3,20 +3,20 @@ open Dream
 open Caqti_lwt
 
 module DB = Database
-open Config
 module API = Api
 
 let logger = Logs.Src.create "mlocid" ~doc:"Mlocid application"
 let () = Logs.Src.set_level logger (Some Logs.Info)
 
-let init_database config =
-  let uri = Printf.sprintf "sqlite3:%s" (database_path config) in
+let init_database (config : Config.config) =
+  let uri = Printf.sprintf "sqlite3:%s" (Config.database_path config) in
   let* connection = Caqti_lwt_unix.connect (Uri.of_string uri) in
   match connection with
   | Ok (module Db : Caqti_lwt.CONNECTION) ->
-    let* () = DB.init_db (module Db) in
-    Lwt.return (Ok (module Db : Caqti_lwt.CONNECTION))
-  | Error e -> Lwt.return (Error (Caqti_error.show e))
+      let* () = DB.init_db (module Db) in
+      Lwt.return (Ok (module Db : Caqti_lwt.CONNECTION))
+  | Error e ->
+      Lwt.return (Error (Caqti_error.show e))
 
 let router db =
   Dream.router [
@@ -65,8 +65,8 @@ let () =
       let app = Dream.logger @@
                 Dream.memory_sessions @@
                 router db in
-      let port = port config in
-      let host = host config in
+      let port = Config.port config in
+      let host = Config.host config in
       Logs.info (fun m -> m "Starting mlocid server on %s:%d" host port);
       Dream.run ~interface:host ~port app
   | Error msg ->
